c93d0153c17b14cf241e0448221b2804dc62b3f2
Diarra Hamadoun <111545347+LALPHASS@users.noreply.github.com>
2023-10-02 23:18:57 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,34 @@
 import streamlit as st
-import lgcn_cd as lch
+
+#################
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def generate_pet_name(animal_type, pet_color, openai_api_key):
+    llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+
+    prompt_template_name = PromptTemplate(
+        input_variables = ['animal_type','pet_color'],
+        template = "I have a {animal_type} pet and I want a cool name for it, it is {pet_color} in color. Suggest me five cool names for my pet."
+    )
+
+    name_chain = LLMChain(llm=llm, prompt=prompt_template_name, output_key="pet_name")
+
+    response = name_chain({'animal_type': animal_type, 'pet_color': pet_color})
+
+
+    return response
+
+################
+
+
 
 #App tille 
 st.title("Pets Name Generator")
@@ -37,6 +66,6 @@ if pet_color:
        st.info("Check your OpenAI API key: it may be too short or too long.")
        st.stop()
 
-    response = lch.generate_pet_name(animal_type, pet_color, openai_api_key)
+    response = generate_pet_name(animal_type, pet_color, openai_api_key)
     st.text(response['pet_name'])
                      
